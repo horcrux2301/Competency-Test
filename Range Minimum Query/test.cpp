58ab3e785b34d7890313539b5f406ded99b7348a
Harsh Khajuria <horcrux2301@yahoo.com>
2018-02-22 02:27:46 +0530

    Added better tests

---
+++ Range Minimum Query/test.cpp
@@ -0,0 +1,86 @@
+#define BOOST_TEST_MODULE test
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/lexical_cast.hpp>
+#include "SegmentTree.h"
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(RangeMinTestCase)
+{
+  // Create a segment tree
+  SegmentTree<int, std::function<int (int, int)> > seg;
+  int arr[] = {4,9,11,4,5,4,10,3};
+  int s = 0;
+  int e = 7;
+  seg.InitSegmentTree(arr, s, e, [](int a,int b) -> int { return a<b ? a : b; });
+
+  // Test cases
+  vector<pair<int,int> > v;
+  v.push_back(make_pair(0,4));
+  v.push_back(make_pair(1,3));
+  v.push_back(make_pair(5,7));
+  v.push_back(make_pair(7,7));
+  v.push_back(make_pair(3,7));
+
+  // Checking test cases
+
+  for(int i=0;i<v.size();i++){
+    int ComputedValue = seg.PrintComputedValue(v[i].first, v[i].second);
+    int tempValue = arr[v[i].first];
+    for(int j=v[i].first+1;j<v[i].second+1;j++){
+      tempValue=min(arr[j],tempValue);
+    }
+    if( ComputedValue != tempValue ){
+      std::string error = "Range Test Case Failed on Test Case " + boost::lexical_cast<std::string>(i);
+      BOOST_ERROR(error);
+    }
+  }
+}
+
+
+BOOST_AUTO_TEST_CASE(RangeMinTestCaseAfterUpdate)
+{
+  // Create a segment tree
+  SegmentTree<int, std::function<int (int, int)> > seg;
+  int arr[] = {4,9,11,4,5,4,10,3};
+  int s = 0;
+  int e = 7;
+  seg.InitSegmentTree(arr, s, e, [](int a,int b) -> int { return a<b ? a : b; });
+
+
+  //Updated the values
+  seg.UpdateSegmentTree(0,-4);
+  seg.UpdateSegmentTree(4,15);
+  seg.UpdateSegmentTree(7,91);
+
+  // Updating the array. If you comment the lines below
+  // errors will be shown on all the test cases below.
+  arr[0]=-4;
+  arr[4]=15;
+  arr[7]=91;
+
+  // Test cases
+  vector<pair<int,int> > v;
+  v.push_back(make_pair(0,4));
+  v.push_back(make_pair(1,3));
+  v.push_back(make_pair(5,7));
+  v.push_back(make_pair(7,7));
+  v.push_back(make_pair(3,7));
+
+  // Checking test cases
+
+  for(int i=0;i<v.size();i++){
+    int ComputedValue = seg.PrintComputedValue(v[i].first, v[i].second);
+    int tempValue = arr[v[i].first];
+    for(int j=v[i].first+1;j<v[i].second+1;j++){
+      tempValue=min(arr[j],tempValue);
+    }
+    if( ComputedValue != tempValue ){
+      std::string error = "Range Test Case Failed on Test Case " + boost::lexical_cast<std::string>(i);
+      BOOST_ERROR(error);
+    }
+  }
+
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "SegmentTree.h"
+
+int main(){
+  // Enter your code here.
+  return 0;
+}
